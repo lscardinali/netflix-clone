7f9c67308f92dbe7bc091d82c1c20c7d6c55c9c7
Lucas Salton Cardinali <lucascardinali92@gmail.com>
2024-02-24 00:34:35 +0000

    Initial Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable, View, StyleSheet } from 'react-native';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { Entypo, Feather, Ionicons } from '@expo/vector-icons';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -29,29 +30,46 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Entypo size={28} name="home" color={color} />,
+          headerTransparent: true,
+          headerBackground: (headerStyle) => <View style={[StyleSheet.absoluteFill, { backgroundColor: "#000000AA", width: "100%" }]} />,
           headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+            <View style={{ flexDirection: "row" }}>
+              <Link href="/modal" asChild>
+                <Pressable>
+                  {({ pressed }) => (
+                    <Feather
+                      name="cast"
+                      size={25}
+                      color={Colors[colorScheme ?? 'light'].text}
+                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                    />
+                  )}
+                </Pressable>
+
+              </Link>
+              <Link href="/modal" asChild>
+                <Pressable>
+                  {({ pressed }) => (
+                    <Feather
+                      name="search"
+                      size={25}
+                      color={Colors[colorScheme ?? 'light'].text}
+                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                    />
+                  )}
+                </Pressable>
+              </Link>
+            </View>
           ),
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Games',
+          tabBarIcon: ({ color }) => <Ionicons name="game-controller-outline" size={28} color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,56 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Image, ScrollView } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabOneScreen() {
+
+  const series = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+  const mockImage = "https://pbs.twimg.com/media/GFetdZGW0AAnKQI.jpg"
+  const insets = useSafeAreaInsets();
+
+  const ItemThumbnail = ({ item }: { item: number }) => {
+    return (
+      <View style={{ maxHeight: 160, maxWidth: 120, marginRight: 8, borderRadius: 8, overflow: 'hidden', alignItems: 'center' }}>
+        <Image style={{ height: 160, width: 120, borderRadius: 8, overflow: 'hidden' }} source={{ uri: mockImage }} />
+
+        <Text style={{ position: 'absolute', bottom: 0, backgroundColor: "#E60710", textAlign: 'center', padding: 4, fontWeight: 'bold', borderTopLeftRadius: 4, borderTopRightRadius: 4, fontSize: 12 }}>
+          Recently Added
+        </Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={[{
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+    }]}>
+      <ScrollView>
+        <Text style={styles.title}>Top Picks for You</Text>
+        <FlatList data={series} renderItem={ItemThumbnail} horizontal style={{ paddingHorizontal: 16, flexGrow: 0, }} />
+        <Text style={styles.title}>Because you watched Seinfeld</Text>
+        <FlatList data={series} renderItem={ItemThumbnail} horizontal style={{ paddingHorizontal: 16, flexGrow: 0, }} />
+        <Text style={styles.title}>Because you watched Seinfeld</Text>
+        <FlatList data={series} renderItem={ItemThumbnail} horizontal style={{ paddingHorizontal: 16, flexGrow: 0, }} />
+        <Text style={styles.title}>Because you watched Seinfeld</Text>
+        <FlatList data={series} renderItem={ItemThumbnail} horizontal style={{ paddingHorizontal: 16, flexGrow: 0, }} />
+        <Text style={styles.title}>Because you watched Seinfeld</Text>
+        <FlatList data={series} renderItem={ItemThumbnail} horizontal style={{ paddingHorizontal: 16, flexGrow: 0, }} />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
   title: {
     fontSize: 20,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
     fontWeight: 'bold',
   },
   separator: {
